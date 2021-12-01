fd8bf2629417edc892e27fce29b8089feadae0bd
Luis Eduardo <luisroyo25@gmail.com>
2021-12-01 10:22:37 -0300

    Preparando a navegação de telas de pesquisa de venda.

---
+++ VendasWebMVC/Controllers/VendaRegistradaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VendasWebMVC.Controllers
+{
+    public class VendaRegistradaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult BuscaSimples()
+        {
+            return View();
+        }
+        public IActionResult BuscaEmGrupo()
+        {
+            return View();
+        }
+    }
+}
